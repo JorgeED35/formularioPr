19f7bc796726ef5062286c65215211824e48a5a6
JorgeED35 <jomarencarnacion3@gmail.com>
2022-08-02 15:17:34 -0500

    fetch

--- script.js
+++ script.js
@@ -1,66 +1,24 @@
-const posts = [
-    {
-        userId: 1,
-        id: 1,
-        title:"Jomar",
-        body:
-            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-    {
-        userId: 1,
-        id: 2,
-        title: "Isabel",
-        body:
-            "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-        userId: 1,
-        id: 3,
-        title: "Piter",
-        body:
-            "pitel\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-    },
-];
+const url = 'https://jsonplaceholder.typicode.com/posts/'
 
-const findPostId = (id) => 
+/* fetch(url) //introduccion al fetch
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(e => console.log(e))
+.finally(() => console.log("Finalizo empty")) */
 
-    new Promise((resolve, reject) => {
-            setTimeout(() => {
-            const post = posts.find((item) => item.id === id)
-            if(post){
-                resolve(post)
-            }else{
-                reject("No se encuentra ningun usuario referente a este id: " + id)
-            }
+const findPostById = async (id) => {
 
-        }, 2000);
+    try {
+        const res = await fetch(url + id)
+        const post = await res.json()
 
-    })
-
-
-/* findPostId (2)
-.then((post) => {
-    console.log(post)})
-.catch(e => console.log(e)); */
-
-const buscar = async (id) => {
-try {
-    const resPost = await Promise.all([
-        findPostId(1),
-        findPostId(2)
-    ])
-    console.log(resPost[1].title, resPost[0].title)
-
-    //const post1 = await findPostId(1)
-    //const post2 = await findPostId(2)
-    //console.log(post2.title, post1.title)
+        console.log(post)
     } catch (error) {
-    console.log(error)
-} finally{
-    console.log("se ejcuta si o si")
-}
+        
+       
+    }finally{
+        console.log("Yo ahi estare IPA")
+    }
 
 }
-buscar(4)
-
-console.log("Fin del codigo jj")
+findPostById(101)
