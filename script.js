9db837f9f00f8935598c17635132086a81f66363
JorgeED35 <jomarencarnacion3@gmail.com>
2022-08-04 12:34:49 -0500

    Api RandM

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-const url = 'https://jsonplaceholder.typicode.com/posts/'
+//Invocando los elelmentos a cambiar con JS
+const loading = document.querySelector('#loading')
 
-/* fetch(url) //introduccion al fetch
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(e => console.log(e))
-.finally(() => console.log("Finalizo empty")) */
+//addEventListener para que al momento de la ejecucion este tenga que esperar 
+document.addEventListener('DOMContentLoaded', () => {
+    fetchApi()
+    
+})
 
-const findPostById = async (id) => {
-
+//async y await para implementar una promesa como metodo para consumir la API
+const fetchApi = async () => {
+//    console.log('Jomar Player w Js')
     try {
-        const res = await fetch(url + id)
-        const post = await res.json()
+        loadingStatus(true)
+        
+        const res = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await res.json()
 
-        console.log(post)
+       //console.log(data)
+       pintarData(data)
     } catch (error) {
         
-       
-    }finally{
-        console.log("Yo ahi estare IPA")
+    } finally{
+         loadingStatus(false)
     }
-
 }
-findPostById(101)
+
+const pintarData = data =>{
+    const cards = document.querySelector('#card-dina')
+    const templatesCard = document.querySelector('#template-card').content
+    const fragment = document.createDocumentFragment()
+    
+    data.results.forEach(item => {
+       //console.log(item)
+       const clone = templatesCard.cloneNode(true)
+       clone.querySelector('h5').textContent = item.name
+       clone.querySelector('p').textContent = item.species
+       clone.querySelector('img').setAttribute("src", item.image)
+
+       //Guardado en el frangment para evitar el reflow 
+       fragment.appendChild(clone)
+    });
+
+    cards.appendChild(fragment)
+}
+//Pintar el loading  
+const loadingStatus = (status) => {
+    if (status) {
+        loading.classList.remove('d-none')
+    } else {
+        loading.classList.add('d-none')
+            }
+}
